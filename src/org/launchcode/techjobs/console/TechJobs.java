2fe75b1da48ed6fba5ed19bd59ba387a355d0447
unknown <smkreter@gmail.com>
2020-08-23 12:20:26 -0500

    added a for loop, i feel confused still, womp womp

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -110,7 +110,9 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
+        for (int i = 0; i < someJobs.size(); i++){
+            System.out.println(someJobs[i]);
+        }
 
-        System.out.println("printJobs is not implemented yet");
     }
 }
