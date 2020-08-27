04e675e6c7b4ad6d0d734856847045a44e23fed1
unknown <smkreter@gmail.com>
2020-08-26 19:27:46 -0500

    added case insensitivity to the search by data type search

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -74,7 +74,7 @@ public class JobData {
 
         for (HashMap<String, String> row : allJobs) {
 
-            String aValue = row.get(column);
+            String aValue = row.get(column).toLowerCase();
 
             if (aValue.contains(value)) {
                 jobs.add(row);
